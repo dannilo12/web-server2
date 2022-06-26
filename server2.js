fbc66dce701c0cd8f7f18e2db1a88230840590f2
danteking1@gmail.comclear <danteking1@gmail.com>
2022-06-26 02:55:35 -0300

    foi

---
+++ server2.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+//const { hostname } = require('os');
+
+hostname = "127.0.0.1";
+port = "3000";
+
+const mimetype = {
+    htlm:"text/html",
+    css:"text/css",
+    js:"text/javascript",
+    png:"image/png",
+    jpeg:"image/jpeg",
+    jpg:"image/jpg",
+    woff:"font/woof"
+};
+
+
