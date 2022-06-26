4a31d5267e7cd9e76e8981a970c5e9e6472ce17e
danteking1@gmail.comclear <danteking1@gmail.com>
2022-06-26 15:24:24 -0300

    correções e bug

--- server2.js
+++ server2.js
@@ -3,7 +3,7 @@
 const http = require('http'); // http é o módulo que trata as entradas da camanda de aplicação em redes
 const url = require('url'); //url é o módulo que trata da construção de urls
 const fs = require('fs'); //fs é o módulo que trata a chamada e manipulação de arquivos no servidor
-const path = require('path'); //
+const path = require('path'); //trata os caminhos dos arquivos do servidor
 
 //Chamado do servidor
 
@@ -15,42 +15,41 @@ const mimeTypes = {
     htlm:"text/html",
     css:"text/css",
     js:"text/javascript",
-    //php:"text/php",
     png:"image/png",
     jpeg:"image/jpeg",
     jpg:"image/jpg",
     woff:"font/woof"
 };
 
-//Criação do servidor
-http.createServer((req,res) => {
+//Criação do servidor com obejtos de requisção (req) e respostas (res)
+http.createServer((req, res) => {
     let acesso_uri = url.parse(req.url).pathname; //Declaração da variável acesso_uri. a URI é um termo que trata de forma geral os caminhos dos diretórios do servidor
 
-    let caminho_de_acesso = path.join(process.cwd(), decodeURI(acesso_uri));
-    console.log(caminho_de_acesso);
+    let caminho_do_recurso = path.join(process.cwd(), decodeURI(acesso_uri));
+    console.log(caminho_do_recurso);
 
     let recurso_carregado;
-    //try {
-        recurso_carregado =  fs.lstatSync(caminho_de_acesso);
-    /*} catch (erro) {
-        res.writeHead(404, {'Content-Typ':'text/plain'});
+    //tratametno da sessão do sistema
+    try {
+        recurso_carregado =  fs.lstatSync(caminho_do_recurso);
+    } catch (erro) {
+        res.writeHead(404, {'Content-Typ': 'text/plain'});
         res.write('Error 404! Arquivo não encontrado!');
         res.end();
-
-    }*/
-
+    }
+    //tratametno do tipo de arquivo requisitado 
     if (recurso_carregado.isFile()) {
-       let mimeType = mimeTypes[path.extname(caminho_de_acesso).substring(1)];
+       let mimeType = mimeTypes[path.extname(caminho_do_recurso).substring(1)];
 
        res.writeHead(200, {'Content-Type': mimeType});
-       let fluxo_arquivo = fs.createReadStream(caminho_de_acesso);
-       fluxo_arquivo.pipe(res);
+       let fluxo_arquivo = fs.createReadStream(caminho_do_recurso); //metodo createReadStream carrega o arquivo
+       fluxo_arquivo.pipe(res); //transmite a informação ao cliente ecapsulado dentro do objeto de resposra (res)
 
-    } else if (recurso_carregado.isDirectory()){
+    } else if(recurso_carregado.isDirectory()){
         res.writeHead(302, {'Location': 'index.html'});
         res.end();
 
-    } else {
+    }else{
         res.writeHead(500, {'Content-Type': 'text/plain'});
         res.write("500: Erro Interno do Servidor!");
         res.end();
